a650db92cc29278849358bd9d60d72306d9089dd
Amy Farley <farley@Amys-MacBook-Pro.local>
2020-12-07 10:21:47 -0600

    updates

--- ui/app/e2e/protractor-ci.conf.js
+++ ui/app/e2e/protractor-ci.conf.js
@@ -3,7 +3,7 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--no-sandbox']
+    args: ['--headless', '--no-sandbox', '--disable-gp', '--window-size=800x600']
   }
 };
 
