6438fc5ba746e28ae226fca2ef1f58c78e000a9f
Amy Farley <farley@Amys-MacBook-Pro.local>
2020-12-07 10:32:10 -0600

    updates for travis

--- ui/app/e2e/protractor-ci.conf.js
+++ ui/app/e2e/protractor-ci.conf.js
@@ -1,7 +1,7 @@
 const config = require('./protractor.conf').config;
 
 config.capabilities = {
-  browserName: 'chrome',
+  browserName: 'firefox',
   chromeOptions: {
     args: ['--headless', '--no-sandbox', '--disable-gp', '--window-size=800x600']
   }
